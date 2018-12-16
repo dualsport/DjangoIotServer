97158a168fef170c54de585a9286c7e11e72ab86
Conrad Eggan <conrade@redcatmfg.com>
2018-12-16 13:25:18 -0600

    Add created_on and updated_on fields to admin views

--- IOT_Server/api/admin.py
+++ IOT_Server/api/admin.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from api.models import Devices, Tags, ValueTypes
 
 class DeviceAdmin(admin.ModelAdmin):
-    list_display = ('device_id', 'name', )
+    list_display = ('device_id', 'name')
+    readonly_fields = ('created_on', 'updated_on',)
 admin.site.register(Devices, DeviceAdmin)
 
 class TagAdmin(admin.ModelAdmin):
     list_display = ('tag_id', 'name', 'device_concat')
+    readonly_fields = ('created_on', 'updated_on',)
 admin.site.register(Tags, TagAdmin)
 
 class ValueTypeAdmin(admin.ModelAdmin):
     list_display = ('value_type_id', 'name')
+    readonly_fields = ('created_on', 'updated_on',)
 admin.site.register(ValueTypes, ValueTypeAdmin)
