4ee39de358dad134004c7d0439b3fd83323e5983
Conrad Eggan <conrade@redcatmfg.com>
2019-01-06 07:43:03 -0600

    Changed plural URLs to singular, e.g. devices >> device.

--- IOT_Server/api/urls.py
+++ IOT_Server/api/urls.py
@@ -9,13 +9,13 @@ from api import views
 
 
 urlpatterns = [
-    path('devices/', views.DeviceList.as_view()),
+    path('device/', views.DeviceList.as_view()),
     path('device/<pk>/', views.DeviceDetail.as_view()),
-    path('devicetags/', views.DeviceTagList.as_view()),
+    path('devicetag/', views.DeviceTagList.as_view()),
     path('devicetag/<device_id>/', views.DeviceTagList.as_view()),
-    path('valuetypes/', views.ValTypeListCreate.as_view()),
+    path('valuetype/', views.ValTypeListCreate.as_view()),
     path('valuetype/<pk>/', views.ValTypeDetail.as_view()),
-    path('tags/', views.TagList.as_view()),
+    path('tag/', views.TagList.as_view()),
     path('tag/<pk>/', views.TagDetail.as_view()),
     path('tagupdate/', views.TagData.as_view()),
     path('tagdata/', views.TagDataList.as_view()),
